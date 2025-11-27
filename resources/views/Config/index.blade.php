60e6062394539ea39629f1c3a363449a8a6c0b3e
AA-07 <firmansyahaziz38@gmail.com>
2025-11-27 11:02:13 +0700

    design fix

--- resources/views/Config/index.blade.php
+++ resources/views/Config/index.blade.php
@@ -3,15 +3,15 @@
 <form method="post" action="{{ url('/') }}" class="items-center margin-top-10">
     @csrf
     <div>
-        <label>nik</label>
+        <div class="flex"><label class="mr-0">NIK</label></div>
         <input name="nik" type="text" class="border p-1 w-full"/>
     </div>
     <div class="mt-2">
-        <label>nama</label>
+        <div class="flex"><label class="mr-0">Nama</label></div>
         <input name="nama"  type="text"  class="border p-1 w-full"/>
     </div>
     <div class="mt-3">
-        <button type="submit"  class="px-3 py-1 border">login</button>
+        <button type="submit"  class="px-5 py-1 border">login</button>
     </div>
 </form>
 @endsection
